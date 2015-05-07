e80f89ca9d2c394b5650fd6f1a71b999fe6b002d
402-a1 <402-a1@402-a1-PC>
2015-05-07 17:12:47 +0900

    자유게시판.. 시간표 일부 코드

---
+++ project/src/timeTable/weboutput.java
@@ -0,0 +1,55 @@
+package timeTable;
+
+public class weboutput {
+
+	private int time;
+	private String mon;
+	private String thu;
+	private String wed;
+	private String tues;
+	private String fri;
+	private String sat;
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public String getMon() {
+		return mon;
+	}
+	public void setMon(String mon) {
+		this.mon = mon;
+	}
+	public String getThu() {
+		return thu;
+	}
+	public void setThu(String thu) {
+		this.thu = thu;
+	}
+	public String getWed() {
+		return wed;
+	}
+	public void setWed(String wed) {
+		this.wed = wed;
+	}
+	public String getTues() {
+		return tues;
+	}
+	public void setTues(String tues) {
+		this.tues = tues;
+	}
+	public String getFri() {
+		return fri;
+	}
+	public void setFri(String fri) {
+		this.fri = fri;
+	}
+	public String getSat() {
+		return sat;
+	}
+	public void setSat(String sat) {
+		this.sat = sat;
+	}
+
+}
